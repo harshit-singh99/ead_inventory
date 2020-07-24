55fdc2b5f8b3f1ec3ffdb2c92667913873b21c6f
Harshit Singh <singh.harshit262@gmail.com>
2020-07-24 15:28:10 +0530

    bug fixex

--- routes/quality.js
+++ routes/quality.js
@@ -5,11 +5,18 @@ const router = express.Router();
 
 
 router.post("/", (req, res) => {
-    console.log("hi");
-    console.log(req.files);
+    console.log(req.body);
+    
     itemName = req.body.name;
-    itemPhoto = req.files.image;
+    try {
+        console.log(req.files);
+        itemPhoto = req.files.image;    
+    } catch (error) {
+        console.log("error in file");
+    }
+    
     let qual = Math.random() * 15 + 84;
+    qual = Math.round(qual * 100) / 100;
     console.log(qual);
     items.findOne({name:itemName}, (err, response) => {
         if (err) return res.status(400).send(err);
@@ -17,24 +24,24 @@ router.post("/", (req, res) => {
             let item = new items({
                 name: req.body.name,
                 category: req.body.category,
-                quantity: req.body.quantity,
+                quantity: Number(req.body.quantity),
                 quality: qual,
-                price: req.body.price * qual,
+                price: Math.round(100 * Number(req.body.price) * qual / 100) / 100,
             });
             item.save((err, newItem) => {
                 if (err) return res.status(400).send(err);
-                res.status(200).send(item);
+                res.status(200).send({price: response.price * response.quantity});
             });
         }
         else{
-            let newQty = response.quantity + req.body.quantity;
-            let newQul = (response.quality * response .quantity + qual * req.body.quantity) / (response.quantity + req.body.quantity);
-            let newPrice = (response.price * response.quantity + qual *req.body.price * req.body.quantity) / (response.quantity + req.body.quantity);
-            response.quantity = newQty;
-            response.quality = newQul;
-            response.price = newPrice;
+            let newQty = Number(response.quantity) + Number(req.body.quantity);
+            let newQul = (response.quality * response .quantity + qual * Number(req.body.quantity) / 100) / newQty;
+            let newPrice = (response.price * response.quantity + qual * Number(req.body.price) * Number(req.body.quantity) / 100 ) / newQty;
+            response.quantity = Math.round(newQty * 100) / 100;
+            response.quality = Math.round(newQul * 100) / 100;
+            response.price = Math.round(newPrice * 100) / 100;
             response.save();
-            res.status(200).send(req.body);
+            res.status(200).send({price: response.price * Number(req.body.quantity)});
         }
     });
 });
